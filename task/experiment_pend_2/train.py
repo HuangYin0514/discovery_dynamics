e74396efe4c9fa704518f3d7c7ca52d06fb45fc7
huangyin <1071324110@qq.com>
2022-12-20 21:35:19 +0800

    commit

--- task/experiment_pend_2/train.py
+++ task/experiment_pend_2/train.py
@@ -43,7 +43,8 @@ def run():
 
     # seed
     ln.utils.init_random_state(args.seed)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
     print('Using the device is:', device)
 
     # data
