de3db71f6f27cff62ee78e0015e95810738f3bb5
huangyin <1071324110@qq.com>
2022-12-21 09:37:36 +0800

    commit

--- learner/criterion/L2_norm_loss.py
+++ learner/criterion/L2_norm_loss.py
@@ -6,6 +6,7 @@ import torch
 
 
 def L2_norm_loss(x, y):
+    print("l2")
     num_examples = x.size()[0]
     diff_norms = torch.norm(x.reshape(num_examples, -1) - y.reshape(num_examples, -1), 2, 1)
     y_norms = torch.norm(y.reshape(num_examples, -1), 2, 1)
