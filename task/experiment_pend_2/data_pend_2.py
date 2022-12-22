0e5e60d8291459c5ec4c8498ea576c3f0341ac80
huangyin <1071324110@qq.com>
2022-12-22 23:57:20 +0800

    commit

--- task/experiment_pend_2/data_pend_2.py
+++ task/experiment_pend_2/data_pend_2.py
@@ -16,8 +16,6 @@ class PendulumData(ln.Data):
         self.l = l
         self.g = 9.8
 
-        self.space = [0, 2 * np.pi, -1, 1]  # value range of q and p
-
         self.obj = obj
         self.dim = dim
         self.dof = obj * self.dim  # degree of freedom
@@ -29,12 +27,6 @@ class PendulumData(ln.Data):
 
     def Init_data(self):
         self.__init_data()
-
-    # def hamilton_right_fn(self, t, coords):
-    #     grad_ham = autograd.grad(self.hamilton_energy_fn)
-    #     grad = grad_ham(coords)
-    #     q, p = grad[self.dof:], -grad[:self.dof]
-    #     return np.asarray([q, p]).reshape(-1)
 
     def hamilton_right_fn(self, t, coords):
         """获取导数"""
@@ -50,6 +42,29 @@ class PendulumData(ln.Data):
         dp1 = -(m1 + m2) * g * l1 * np.sin(q1) - h1 + h2 * np.sin(2 * (q1 - q2))
         dp2 = -m2 * g * l2 * np.sin(q2) + h1 - h2 * np.sin(2 * (q1 - q2))
         return np.asarray([dq1, dq2, dp1, dp2]).reshape(-1)
+
+    def hamilton_right_fn2(self, t, coords):
+        grad_ham = autograd.grad(self.hamilton_energy_fn)
+        grad = grad_ham(coords)
+        q, p = grad[self.dof:], -grad[:self.dof]
+        return np.asarray([q, p]).reshape(-1)
+
+    def hamilton_energy_fn(self, coords):
+        """能量函数"""
+        # From "The double pendulum: Hamiltonian formulation"
+        # https://diego.assencio.com/?index=e5ac36fcb129ce95a61f8e8ce0572dbf
+        # H = self.hamiltonian_kinetic(coords) + self.hamiltonian_potential(coords)  # some error in this implementation
+        q1, q2, p1, p2 = np.split(coords, 4)  # q is angle, p is angular momentum.
+        l1, l2, m1, m2 = self.l[0], self.l[1], self.m[0], self.m[1]
+        H = (m1 + m2) * self.g * l1 * (-np.cos(q1)) + m2 * self.g * l2 * (-np.cos(q2)) \
+            + ((m1 + m2) * l1 ** 2 * p2 ** 2 + m2 * l2 ** 2 * p1 ** 2 - 2 * m2 * l1 * l2 * p1 * p2 * np.cos(q1 - q2)) / \
+            (2 * m2 * (l1 ** 2) * (l2 ** 2) * (m1 + m2 * np.sin(q1 - q2) ** 2))
+        return H
+
+    def hamilton_energy_fn2(self, coords):
+        """能量函数"""
+        H = self.hamiltonian_kinetic(coords) + self.hamiltonian_potential(coords)  # some error in this implementation
+        return H
 
     def hamiltonian_kinetic(self, coords):
         assert (len(coords) == self.dof * 2)
@@ -71,153 +86,31 @@ class PendulumData(ln.Data):
             U = U + self.m[i] * g * y
         return U
 
-    def hamilton_energy_fn(self, coords):
-        """能量函数"""
-        # From "The double pendulum: Hamiltonian formulation"
-        # https://diego.assencio.com/?index=e5ac36fcb129ce95a61f8e8ce0572dbf
-        # H = self.hamiltonian_kinetic(coords) + self.hamiltonian_potential(coords)  # some error in this implementation
-        q1, q2, p1, p2 = np.split(coords, 4)  # q is angle, p is angular momentum.
-        l1, l2, m1, m2 = self.l[0], self.l[1], self.m[0], self.m[1]
-        H = (m1 + m2) * self.g * l1 * (-np.cos(q1)) + m2 * self.g * l2 * (-np.cos(q2)) \
-            + ((m1 + m2) * l1 ** 2 * p2 ** 2 + m2 * l2 ** 2 * p1 ** 2 - 2 * m2 * l1 * l2 * p1 * p2 * np.cos(q1 - q2)) / \
-            (2 * m2 * (l1 ** 2) * (l2 ** 2) * (m1 + m2 * np.sin(q1 - q2) ** 2))
-        return H
-
-    # def random_config(self, space, num):
-    #     qmin, qmax, pmin, pmax = space[0], space[1], space[2], space[3]
-    #     x0 = np.zeros((num, self.obj * 2))
-    #     for i in range(self.obj):
-    #         q = np.random.rand(num) * (qmax - qmin) + qmin
-    #         p = np.random.rand(num) * (pmax - pmin) + pmin
-    #         x0[..., i] = q
-    #         x0[..., i + self.obj] = p
-    #     return x0
-    def random_config(self, system='hnn'):
-        max_momentum = 1.
-        y0 = np.zeros(self.obj * 2)
-        for i in range(self.obj):
-            theta = (2 * np.pi - 0) * np.random.rand() + 0
-            momentum = (2 * np.random.rand() - 1) * max_momentum
-            y0[i] = theta
-            y0[i + self.obj] = momentum
-        return y0.reshape(-1)
+    def random_config(self, num):
+        x0_list = []
+        for _ in range(num):
+            max_momentum = 1.
+            x0 = np.zeros(self.obj * 2)
+            for i in range(self.obj):
+                theta = (2 * np.pi - 0) * np.random.rand() + 0  # [0, 2pi]
+                momentum = (2 * np.random.rand() - 1) * max_momentum  # [-1, 1]*max_momentum
+                x0[i] = theta
+                x0[i + self.obj] = momentum
+            x0_list.append(x0)
+        return np.asarray(x0_list)
 
     def __init_data(self):
-        # self.X_train, self.y_train = self.__generate_random(self.space, self.train_num, self.h)
-        # self.X_test, self.y_test = self.__generate_random(self.space, self.test_num, self.h)
+        self.X_train, self.y_train = self.__generate_random(self.train_num, self.h)
+        self.X_test, self.y_test = self.__generate_random(self.test_num, self.h)
 
-        data = self.get_dataset(seed=0, system='hnn', noise_std=0, samples=100)
-        self.X_train, self.y_train = data['x'], data['dx']
-        self.X_test, self.y_test = data['test_x'], data['test_dx']
-
-    def __generate_random(self, space, num, h):
-        x0 = self.random_config(space, num)
+    def __generate_random(self, num, h):
+        x0 = self.random_config(num)
         X = self.__generate(x0, h)
         X = np.concatenate(X)
-        # y = list(map(lambda x: self.hamilton_right_fn(None, x), X))
-        # y = np.asarray(y)
-        dydt = [self.hamilton_right_fn(None, y) for y in X]
-        y = np.stack(dydt)
-        # E = np.array([self.hamilton_energy_fn(y) for y in X])
+        y = np.asarray(list(map(lambda x: self.hamilton_right_fn(None, x), X)))
+        E = np.array([self.hamilton_energy_fn(y) for y in X])
         return X, y
 
     def __generate(self, X, h):
         X = np.array(list(map(lambda x: self.solver.solve(x, h), X)))
         return X
-
-    def get_trajectory(self, t0=0, t_end=10, ode_stepsize=None, y0=None, noise_std=0., system="hnn", **kwargs):
-        # get initial state
-        self.m = kwargs['m'] if 'm' in kwargs else [1. for i in range(self.obj)]
-        self.l = kwargs['l'] if 'l' in kwargs else [1. for i in range(self.obj)]
-        self.g = kwargs['g'] if 'g' in kwargs else 9.8
-        if y0 is None:
-            y0 = self.random_config(system)
-
-        if system == "hnn":
-            dynamics_solution = self.solver.solve(y0, ode_stepsize)
-            # dynamics_t, dynamics_solution = self.runge_kutta_solver(self.hamilton_right_fn, t0, t_end, ode_stepsize, y0)
-            dydt = [self.hamilton_right_fn(None, y) for y in dynamics_solution]
-            dydt = np.stack(dydt)
-            E = np.array([self.hamilton_energy_fn(y) for y in dynamics_solution])
-
-            # add noise
-            x = dynamics_solution + np.random.randn(*dynamics_solution.shape) * noise_std
-            return x, dydt, dynamics_solution, E
-
-        elif system == "lnn":
-
-            dynamics_t, dynamics_solution = self.runge_kutta_solver(self.lagrangian_right_fn, t0, t_end, ode_stepsize,
-                                                                    y0)
-            dydt = [self.lagrangian_right_fn(None, y) for y in dynamics_solution]
-            dydt = np.stack(dydt)
-            E = np.array([self.lagrangian_energy_fn(y) for y in dynamics_solution])
-
-            x = dynamics_solution[:, :self.dof]
-            v = dydt[:, :self.dof]
-            a = dydt[:, self.dof:]
-
-            # add noise
-            x += np.random.randn(*x.shape) * noise_std
-            v += np.random.randn(*v.shape) * noise_std
-
-            return x, v, a, dynamics_t, E
-
-        else:
-            raise ValueError('Unsupported system system, choose'
-                             ' system = \'hnn\' or \'modlanet\' instead.')
-
-    def get_dataset(self, seed=0, samples=100, ode_stepsize=0.1, test_split=0.9, system='hnn', **kwargs):
-        data = {'meta': locals()}
-        self.samples = samples
-
-        if system == 'hnn':
-            # randomly sample inputs
-            np.random.seed(seed)
-            xs, dxs, ts, Es = [], [], [], []
-            for s in range(self.samples):
-                x, dx, t, E = self.get_trajectory(system=system, ode_stepsize=ode_stepsize, **kwargs)
-                xs.append(x)
-                dxs.append(dx)
-                ts.append(t)
-                Es.append(E)
-
-            data['x'] = np.concatenate(xs)
-            data['dx'] = np.concatenate(dxs)
-            data['t'] = np.concatenate(ts).reshape(-1, 1)
-            data['E'] = np.concatenate(Es).reshape(-1, 1)
-
-            # make a train/test split
-            split_ix = int(len(data['x']) * test_split)
-            split_data = {}
-            for k in ['x', 'dx', 't', 'E']:
-                split_data[k], split_data['test_' + k] = data[k][:split_ix], data[k][split_ix:]
-            data = split_data
-
-        elif system == 'lnn':
-            # randomly sample inputs
-            xs, vs, acs, ts, Es = [], [], [], [], []
-            for s in range(samples):
-                x, v, ac, t, E = self.get_trajectory(system=system, ode_stepsize=ode_stepsize, **kwargs)
-                xs.append(x)
-                vs.append(v)
-                acs.append(ac)
-                ts.append(t)
-                Es.append(E)
-
-            data['x'] = np.concatenate(xs)
-            data['v'] = np.concatenate(vs)
-            data['ac'] = np.concatenate(acs)
-            data['t'] = np.concatenate(ts).reshape(-1, 1)
-            data['E'] = np.concatenate(Es).reshape(-1, 1)
-
-            # make a train/test split
-            split_ix = int((data['x'].shape[0]) * test_split)
-            split_data = {}
-            for k in ['x', 'v', 'ac', 't', 'E']:
-                split_data[k], split_data['test_' + k] = data[k][:split_ix], data[k][split_ix:]
-            data = split_data
-
-        else:
-            raise ValueError('Unsupported dynamic system, choose'
-                             'system = \'hnn\' or \'modlanet\' instead.')
-        return data
