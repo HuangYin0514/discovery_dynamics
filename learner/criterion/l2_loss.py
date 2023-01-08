bc554d34374c0e47b11a9ee8343049fb02112b67
drhuang <1071324110@qq.com>
2023-01-09 01:58:38 +0800

    changes

--- learner/criterion/l2_loss.py
+++ learner/criterion/l2_loss.py
@@ -3,10 +3,10 @@ import torch
 
 def L2_loss(x, y):
     # norm is sqrt [ (x - y).pow(2).sum(dim) ]
-    # num_examples = x.size()[0]
+    num_examples = x.size()[0]
     # diff_norms = torch.norm(x.reshape(num_examples, -1) - y.reshape(num_examples, -1), 2, 1)
     # res = torch.mean(diff_norms)
-    res = (x - y).pow(2).mean()
+    res = (x.reshape(num_examples, -1) - y.reshape(num_examples, -1)).pow(2).mean()
     return res
 
 
@@ -15,4 +15,3 @@ def L2_norm_loss(x, y):
     diff_norms = torch.norm(x.reshape(num_examples, -1) - y.reshape(num_examples, -1), 2, 1)
     y_norms = torch.norm(y.reshape(num_examples, -1), 2, 1)
     return torch.mean(diff_norms / y_norms)
-
