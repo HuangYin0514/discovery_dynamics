3cb4f02ca56144754a8606be4e00a3440c091294
drhuang <1071324110@qq.com>
2023-01-08 23:56:33 +0800

    changes

--- learner/utils/__init__.py
+++ learner/utils/__init__.py
@@ -2,8 +2,12 @@ from .common_utils import count_parameters
 from .common_utils import dfx
 from .common_utils import get_device, init_random_state
 from .common_utils import lazy_property, timing, deprecated
+
 from .download_file import download_file_from_google_drive
+
 from .net_utils import load_network
 from .net_utils import weights_init_xavier_normal
+
 from .pend_utils import polar2xy, plot_pend_trajectory
 
+from hamiltonian_utils import Ham_J
---
+++ learner/utils/hamiltonian_utils.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+@author: Yin Huang
+@contact: hy1071324110@gmail.com
+@time: 2023/1/8 11:52 PM
+@desc:
+"""
+import torch
+
+
+def ham_J(M):
+    """
+    applies the J matrix to another matrix M.
+    input: M (*,2nd,b)
+
+    J ->  # [ 0, I]
+          # [-I, 0]
+
+    output: J@M (*,2nd,b)
+    """
+    *star, D, b = M.shape
+    JM = torch.cat([M[..., D // 2:, :], -M[..., : D // 2, :]], dim=-2)
+    return JM
