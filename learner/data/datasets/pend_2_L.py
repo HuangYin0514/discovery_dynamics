5b2a9bf90f3cd632637011c4f357053930be899f
drhuang <1071324110@qq.com>
2023-01-21 01:12:50 +0800

    changes

--- learner/data/datasets/pend_2_L.py
+++ learner/data/datasets/pend_2_L.py
@@ -153,7 +153,7 @@ class Pendulum2_L(BaseBodyDataset, nn.Module):
         return eng
 
     def random_config(self):
-        max_momentum = 1.
+        max_momentum = 10.
         x0 = torch.zeros(self._obj * 2)
         for i in range(self._obj):
             theta = (2 * np.pi - 0) * torch.rand(1, ) + 0  # [0, 2pi]
