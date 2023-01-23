ad33e9de4df60c0cf0bd2738e3a2d223f86ef58d
drhuang <1071324110@qq.com>
2023-01-24 01:48:41 +0800

    changes

--- learner/nn/modlanet.py
+++ learner/nn/modlanet.py
@@ -162,7 +162,7 @@ class ModLaNet(LossNN):
         y = 0
         for i in range(self.obj):
             # U += (9.8 * x_global[:, (i) * self.global_dim + 1: (i + 1) * self.global_dim])
-            U += (9.8 * self.mass(self.Potential1(x_global[:, i * self.global_dim: (i + 1) * self.global_dim])))
+            U += (9.8 * self.mass(self.Potential1(x_global[:, i * self.global_dim+ 1: (i + 1) * self.global_dim])))
 
 
         # Calculate the kinetic energy for i-th element
