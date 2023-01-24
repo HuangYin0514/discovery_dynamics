58a26efd888de44db125d86b1b76ca2d73505bee
drhuang <1071324110@qq.com>
2023-01-24 11:24:07 +0800

    changes

--- learner/nn/modlanet.py
+++ learner/nn/modlanet.py
@@ -91,10 +91,6 @@ class ModLaNet(LossNN):
         torch.nn.init.ones_(self.mass.weight)
 
     def forward(self, t, data):
-        # data = data.clone().detach()
-        # data[..., :int(data.shape[-1] // 2)] %= 2 * torch.pi  # pendulum
-        # data = data.clone().detach().requires_grad_(True)
-
         bs = data.size(0)
         x, v = torch.chunk(data, 2, dim=1)
 
