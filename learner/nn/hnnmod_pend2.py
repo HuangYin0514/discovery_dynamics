a67a5537c2bfe0043f4ef24db7c8c4165c56bbd7
drhuang <1071324110@qq.com>
2023-01-24 23:59:19 +0800

    changes

--- learner/nn/hnnmod_pend2.py
+++ learner/nn/hnnmod_pend2.py
@@ -7,13 +7,30 @@
 """
 import numpy as np
 import torch
-from torch import nn
+from torch import nn, Tensor
 
 from .base_module import LossNN
 from .mlp import MLP
+from .utils_nn import CosSinNet, ReshapeNet
 from ..integrator import ODESolver
 from ..utils import dfx, lazy_property
 
+
+class MassNet(nn.Module):
+    def __init__(self, q_dim, num_layers=3, hidden_dim=30):
+        super(MassNet, self).__init__()
+
+        self.cos_sin_net = CosSinNet()
+        self.net = nn.Sequential(
+            MLP(input_dim=q_dim * 2, hidden_dim=hidden_dim, output_dim=q_dim * q_dim, num_layers=num_layers,
+                act=nn.Tanh),
+            ReshapeNet(-1, q_dim, q_dim)
+        )
+
+    def forward(self, q):
+        q = self.cos_sin_net(q)
+        out = self.net(q)
+        return out
 
 class GlobalPositionTransform(nn.Module):
     """Doing coordinate transformation using a MLP"""
@@ -89,6 +106,43 @@ class HnnMod_pend2(LossNN):
 
         self.mass = torch.nn.Linear(1, 1, bias=False)
         torch.nn.init.ones_(self.mass.weight)
+
+        self.mass_net = MassNet(q_dim=self.dof, num_layers=1, hidden_dim=200)
+
+
+    def tril_Minv(self, q):
+        """
+        Computes the inverse of a matrix M^{-1}(q)
+        But only get the lower triangle of the inverse matrix  M^{-1}(q)
+        to get lower triangle of  M^{-1}(q)  = [x, 0]
+                                               [x, x]
+        """
+        mass_net_q = self.mass_net(q)
+        res = torch.triu(mass_net_q, diagonal=1)
+        res = res + torch.diag_embed(
+            torch.nn.functional.softplus(torch.diagonal(mass_net_q, dim1=-2, dim2=-1)),
+            dim1=-2,
+            dim2=-1,
+        )
+        res = res.transpose(-1, -2)  # Make lower triangular
+        return res
+
+    def Minv(self, q: Tensor, eps=1e-4) -> Tensor:
+        """Compute the learned inverse mass matrix M^{-1}(q)
+            M = LU
+            M^{-1} = (LU)^{-1} = U^{-1} @ L^{-1}
+            M^{-1}(q) = [x, 0] @ [x, x]
+                        [x, x]   [0, x]
+        Args:
+            q: bs x D Tensor representing the position
+        """
+        assert q.ndim == 2
+        lower_triangular = self.tril_Minv(q)
+        assert lower_triangular.ndim == 3
+        diag_noise = eps * torch.eye(lower_triangular.size(-1), dtype=q.dtype, device=q.device)
+        Minv = lower_triangular.matmul(lower_triangular.transpose(-2, -1)) + diag_noise
+        return Minv
+
 
     @lazy_property
     def J(self):
