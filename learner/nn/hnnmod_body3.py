bd7b01c8055b1d7d03baf136079234e50e4d1857
drhuang <1071324110@qq.com>
2023-01-27 15:55:20 +0800

    changes

--- learner/nn/hnnmod_body3.py
+++ learner/nn/hnnmod_body3.py
@@ -145,7 +145,7 @@ class HnnMod_body3(LossNN):
             # dp_dt[:, i * self.dim:(i + 1) * self.dim] = self.dynamics_net(q[:, i * self.dim:(i + 1) * self.dim],
             #                                                               p[:, i * self.dim:(i + 1) * self.dim],
             #                                                               dqH[:, i * self.dim:(i + 1) * self.dim])
-            dp_dt[:, i * self.dim:(i + 1) * self.dim] = -dqH[:, i * self.dim:(i + 1) * self.dim]
+            dp_dt[:, i * self.dim:(i + 1) * self.dim] = dqH[:, i * self.dim:(i + 1) * self.dim]
             # dp_dt = self.dynamics_net(q, p)
             dz_dt = torch.cat([dq_dt, dp_dt], dim=-1)
         return dz_dt
