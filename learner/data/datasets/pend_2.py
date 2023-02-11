910b75065e3a237ecbefd2544161708d12ad32f5
drhuang <1071324110@qq.com>
2023-02-11 12:48:23 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -50,7 +50,7 @@ class Pendulum2(BaseBodyDataset, nn.Module):
         _time_step = int((t_end - t0) / self.dt)
         self.t = torch.linspace(t0, t_end, _time_step)
 
-        t_end = 15.
+        t_end = 30.
         _time_step = int((t_end - t0) / self.dt)
         self.test_t = torch.linspace(t0, t_end, _time_step)
 
