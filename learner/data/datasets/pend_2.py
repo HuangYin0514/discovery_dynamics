e2ca53476bbba59ef8e16830de3ffa1dfe7dd1a7
drhuang <1071324110@qq.com>
2023-02-13 01:20:03 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -128,7 +128,7 @@ class Pendulum2(BaseBodyDataset, nn.Module):
         # T = torch.sum(T).reshape(-1)
 
         v = torch.matmul(self.Minv(x), p.unsqueeze(-1) )
-        T = torch.sum(v).reshape(-1)
+        T = torch.sum(v**2).reshape(-1)
 
         return T
 
