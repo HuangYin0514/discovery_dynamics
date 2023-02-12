3966202332b258c92c302d5cdb74d9fbe030f77d
drhuang <1071324110@qq.com>
2023-02-12 22:24:37 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -118,8 +118,9 @@ class Pendulum2(BaseBodyDataset, nn.Module):
         assert len(coords) == self._dof * 2
         x, p = torch.chunk(coords, 2, dim=0)
         # T = 0.5 * p @ self.Minv(x) @ p
-        T = torch.sum(self.Minv(x)).reshape(-1, )
-
+        # T = torch.sum(self.Minv(x)).reshape(-1, )
+        T = torch.matmul(self.Minv(x), p.unsqueeze(-1)).squeeze(-1)
+        T = torch.sum(T).reshape(-1)
         return T
 
     def potential(self, coords):
