fd809108c11bf8ef077d10a5dbed66010a4a5e35
drhuang <1071324110@qq.com>
2023-02-12 20:48:41 +0800

    changes

--- task/analyze.py
+++ task/analyze.py
@@ -52,8 +52,8 @@ def main():
 
     # seed
     ln.utils.init_random_state(args.seed)
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    device = 'cpu'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # device = 'cpu'
     print('Using the device is:', device)
 
     # net ----------------------------------------------------------------
