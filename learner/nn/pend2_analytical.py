788edf066b8fb8c6c08ac02f915b69bcd5a01f3a
drhuang <1071324110@qq.com>
2023-02-12 21:09:57 +0800

    changes

--- learner/nn/pend2_analytical.py
+++ learner/nn/pend2_analytical.py
@@ -94,12 +94,15 @@ class Pend2_analytical(LossNN):
         return dz_dt
 
     def integrate(self, X0, t):
+
         def angle_forward(t, coords):
             x, p = torch.chunk(coords, 2, dim=-1)
             new_x = x % (2 * torch.pi)
             new_coords = torch.cat([new_x, p], dim=-1).clone().detach().requires_grad_(True)
             return self(t, new_coords)
 
-        out = ODESolver(angle_forward, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+        # out = ODESolver(angle_forward, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+        for x0 in X0:
+            out = ODESolver(angle_forward, x0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
 
         return out
