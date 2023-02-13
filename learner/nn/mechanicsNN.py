65891cb83a6e8aa0236a78753d3b7928ac05dcf9
drhuang <1071324110@qq.com>
2023-02-13 16:44:31 +0800

    changes

--- learner/nn/mechanicsNN.py
+++ learner/nn/mechanicsNN.py
@@ -125,13 +125,9 @@ class MechanicsNN(LossNN):
         def angle_forward(t, coords):
             x, p = torch.chunk(coords, 2, dim=-1)
             new_x = x % (2 * torch.pi)
-            new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
+            new_coords = torch.cat([new_x, p], dim=-1).clone().detach().requires_grad_(True)
             return self(t, new_coords)
 
         coords = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
 
-        x, p = torch.chunk(coords, 2, dim=-1)
-        new_x = x % (2 * torch.pi)
-        new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
-
-        return new_coords
+        return coords
