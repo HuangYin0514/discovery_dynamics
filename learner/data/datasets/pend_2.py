04576a341e6195364773215a43e70901d97a154b
drhuang <1071324110@qq.com>
2023-02-13 14:41:43 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -156,12 +156,16 @@ class Pendulum2(BaseBodyDataset, nn.Module):
 
     def generate(self, x0, t):
         print("Generating for new function!")
-
         def angle_forward(t, coords):
             x, p = torch.chunk(coords, 2, dim=0)
             new_x = x % (2 * torch.pi)
             new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
             return self(t, new_coords)
 
-        out = ODESolver(self, x0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
-        return out
+        coords = ODESolver(self, x0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+
+        x, p = torch.chunk(coords, 2, dim=0)
+        new_x = x % (2 * torch.pi)
+        new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
+
+        return new_coords
--- learner/nn/baseline_pend2.py
+++ learner/nn/baseline_pend2.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from ._base_module import LossNN
@@ -23,5 +24,12 @@ class Baseline_pend2(LossNN):
         return out
 
     def integrate(self, X0, t):
-        out = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
-        return out
+        print("Generating for new function!")
+        def angle_forward(t, coords):
+            x, p = torch.chunk(coords, 2, dim=0)
+            new_x = x % (2 * torch.pi)
+            new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
+            return self(t, new_coords)
+
+        coords = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+        return coords
