8511ea649a55d819f73dad304c24f671d5deaa57
drhuang <1071324110@qq.com>
2023-02-13 17:35:42 +0800

    changes

--- learner/nn/baseline_pend2.py
+++ learner/nn/baseline_pend2.py
@@ -29,12 +29,5 @@ class Baseline_pend2(LossNN):
         return out
 
     def integrate(self, X0, t):
-        def angle_forward(t, coords):
-            q, p = torch.chunk(coords, 2, dim=-1)
-            new_q = q % (2 * torch.pi)
-            new_coords = torch.cat([new_q, p], dim=-1).clone().detach().requires_grad_(True)
-            return self(t, new_coords)
-
-        coords = ODESolver(angle_forward, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
-
+        coords = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
         return coords
