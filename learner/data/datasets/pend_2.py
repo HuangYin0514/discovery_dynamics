4c65a7110efe356b10c542a5bc32e01e173a7d13
drhuang <1071324110@qq.com>
2023-02-13 15:32:25 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -164,8 +164,4 @@ class Pendulum2(BaseBodyDataset, nn.Module):
 
         coords = ODESolver(self, x0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
 
-        x, p = torch.chunk(coords, 2, dim=-1)
-        new_x = x % (2 * torch.pi)
-        new_coords = torch.cat([new_x, p], dim=-1).clone().detach()
-
-        return new_coords
+        return coords
