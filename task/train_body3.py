b700234f2edfc304cb1199c5a837e6830f06aee6
drhuang <1071324110@qq.com>
2023-02-14 17:01:03 +0800

    changes

--- task/train_body3.py
+++ task/train_body3.py
@@ -21,7 +21,7 @@ parser.add_argument('--obj', default=3, type=int, help='number of objects')
 parser.add_argument('--dim', default=2, type=int, help='coordinate dimension')
 
 # data
-parser.add_argument('--data_name', default='Body3_L', type=str,
+parser.add_argument('--data_name', default='Body3', type=str,
                     help='choose dataset '
                          '[Pendulum2 Pendulum2_L '
                          'Body3 Body3_L '
