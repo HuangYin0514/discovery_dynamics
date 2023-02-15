03c8235d6ccd7fbd301ad0f89b2535bf0fdff2fe
drhuang <1071324110@qq.com>
2023-02-15 10:47:20 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -140,16 +140,30 @@ class Pendulum2(BaseBodyDataset, nn.Module):
     def random_config(self, num):
         x0_list = []
         for i in range(num):
-            max_momentum = 0.8
+            max_momentum = 10.
             x0 = torch.zeros((self.obj * 2))
             for i in range(self.obj):
-                theta = (0.5 * np.pi) * torch.rand(1, ) + 0  # [0, 2pi]
+                theta = (2 * np.pi) * torch.rand(1, ) + 0  # [0, 2pi]
                 momentum = (2 * torch.rand(1, ) - 1) * max_momentum  # [-1, 1]*max_momentum
                 x0[i] = theta
                 x0[i + self.obj] = momentum
             x0_list.append(x0)
         x0 = torch.stack(x0_list)
         return x0
+
+    # def random_config(self, num):
+    #     x0_list = []
+    #     for i in range(num):
+    #         max_momentum = 10.
+    #         x0 = torch.zeros((self.obj * 2))
+    #         for i in range(self.obj):
+    #             theta = (2 * np.pi) * torch.rand(1, ) + 0  # [0, 2pi]
+    #             momentum = (2 * torch.rand(1, ) - 1) * max_momentum  # [-1, 1]*max_momentum
+    #             x0[i] = theta
+    #             x0[i + self.obj] = momentum
+    #         x0_list.append(x0)
+    #     x0 = torch.stack(x0_list)
+    #     return x0
 
     def generate(self, X0, t):
         print("Generating for new function!")
