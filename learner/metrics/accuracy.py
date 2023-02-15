54ac859d5b1ab2ea8d9e43e3ebf501fd2a31f559
drhuang <1071324110@qq.com>
2023-02-15 16:47:00 +0800

    changes

--- learner/metrics/accuracy.py
+++ learner/metrics/accuracy.py
@@ -17,11 +17,7 @@ def square_err_fn(x, y):
         x_position = x_[..., :dof]
         y_position = y_[..., :dof]
 
-        error = torch.abs(x_position - y_position)
-        error = torch.clamp(error, min=1e-7)
-
-        error_norm = torch.linalg.norm(error)/len(x_position)
-
+        error_norm = (x_position - y_position).norm() / len(x_position)
         err_list.append(error_norm)
 
     position_err = torch.stack(err_list)
@@ -29,12 +25,20 @@ def square_err_fn(x, y):
 
 
 def rel_err_fn(x, y):
-    #  This function is  sqrt（a-b）** 2/ (a**2 + b**2)
-    square_err = ((x - y) ** 2).mean()
-    rel_err = torch.sqrt(square_err) / \
-              (torch.sqrt((x ** 2).mean((-1, -2))) + torch.sqrt((y ** 2).mean((-1, -2))))
-    loggeomean_rel_err = torch.log(torch.clamp(rel_err, min=1e-7))
-    return loggeomean_rel_err
+    bs, times, states = x.shape
+    dof = int(states // 2)
+
+
+    err_list = []
+    for x_, y_ in zip(x, y):
+        x_position = x_[..., :dof]
+        y_position = y_[..., :dof]
+
+        rel_err = (x_position - y_position).norm() / (x_position.norm() + y_position.norm())
+        err_list.append(rel_err)
+
+    err = torch.stack(err_list)
+    return err
 
 
 def energy_err_fn(x, y, energy_function):
@@ -44,15 +48,10 @@ def energy_err_fn(x, y, energy_function):
         eng_y = energy_function(y_)
         # eng_y = eng_y[0].repeat(len(eng_y)) # 与真实的eng对比
 
-        error = torch.abs(eng_x - eng_y)
-        error = torch.clamp(error, min=1e-7)
-        # H_err = torch.abs(eng_x - eng_y) / (torch.abs(eng_x) + torch.abs(eng_y)) # relatively errors 与loss正相关
-        H_err = torch.linalg.norm(error)/len(eng_x)
-
-        err_list.append(H_err)
+        rel_err = (eng_x - eng_y).norm() / (eng_x.norm() + eng_y.norm())
+        err_list.append(rel_err)
 
     E_err = torch.stack(err_list)
-
     return E_err
 
 
@@ -69,4 +68,4 @@ def accuracy_fn(output_traj, target_traj, energy_function):
     mse_err = square_err_fn(output_traj, target_traj)
     rel_err = rel_err_fn(output_traj, target_traj)
     eng_err = energy_err_fn(output_traj, target_traj, energy_function)
-    return mse_err, torch.exp(rel_err), eng_err
+    return mse_err, rel_err, eng_err
