d72234f10b039975771a2085832c1f2edf229262
drhuang <1071324110@qq.com>
2023-02-16 21:05:10 +0800

    changes

--- learner/nn/modlanet_pend2.py
+++ learner/nn/modlanet_pend2.py
@@ -110,13 +110,26 @@ class ModLaNet_pend2(LossNN):
                                                                                              (j + 1) * self.global_dim]
                 v_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim] += v_global[:, (j) * self.global_dim:
                                                                                              (j + 1) * self.global_dim]
-            x_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = self.global4x(
-                x[:, (i) * self.dim: (i + 1) * self.dim],
-                x_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim])
-            v_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = self.global4v(
-                x[:, (i) * self.dim: (i + 1) * self.dim],
-                v[:, (i) * self.dim: (i + 1) * self.dim],
-                v_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim])
+            # x_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = self.global4x(
+            #     x[:, (i) * self.dim: (i + 1) * self.dim],
+            #     x_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim])
+            # v_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = self.global4v(
+            #     x[:, (i) * self.dim: (i + 1) * self.dim],
+            #     v[:, (i) * self.dim: (i + 1) * self.dim],
+            #     v_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim])
+            x_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = \
+                torch.cat(
+                    [torch.sin(x[:, (i) * self.dim: (i + 1) * self.dim]),
+                     torch.cos(x[:, (i) * self.dim: (i + 1) * self.dim])],
+                    dim=-1) + \
+                x_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim]
+
+            v_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = \
+                torch.cat(
+                    [torch.sin(x[:, (i) * self.dim: (i + 1) * self.dim]),
+                     torch.cos(x[:, (i) * self.dim: (i + 1) * self.dim])],
+                    dim=-1) * v[:, (i) * self.dim: (i + 1) * self.dim] + \
+                v_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim]
 
         # Calculate the potential energy for i-th element ------------------------------------------------------------
         U = 0.
