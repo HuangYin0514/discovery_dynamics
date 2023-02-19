ce739e763121cb7691d0ef47bf1063c65ed041ac
drhuang <1071324110@qq.com>
2023-02-20 00:38:43 +0800

    changes

--- learner/nn/modlanet_pend2.py
+++ learner/nn/modlanet_pend2.py
@@ -168,7 +168,7 @@ class ModLaNet_pend2(LossNN):
                 print("i am is none of dxidvL")
             dxdvL[:, i, :] += dxidvL
 
-        dvdvL_inv = torch.linalg.pinv(dvdvL)
+        dvdvL_inv = torch.linalg.inv(dvdvL)
 
         a = dvdvL_inv @ (dxL.unsqueeze(2) - dxdvL @ v.unsqueeze(2))  # (bs, a_dim, 1)
         a = a.squeeze(2)
