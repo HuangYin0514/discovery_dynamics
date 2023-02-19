11302a24030cc67bab441b8920041904ca21521a
drhuang <1071324110@qq.com>
2023-02-20 01:00:05 +0800

    changes

--- learner/nn/modlanet_pend2.py
+++ learner/nn/modlanet_pend2.py
@@ -38,8 +38,8 @@ class GlobalVelocityTransform(nn.Module):
                        act=act)
 
     def forward(self, x, v, v0):
-        y = torch.cat([torch.sin(x), torch.cos(x)], dim=-1) * v + v0
-        # y = self.mlp(x) * v + v0
+        # y = torch.cat([torch.sin(x), torch.cos(x)], dim=-1) * v + v0
+        y = self.mlp(x) * v + v0
 
         return y
 
