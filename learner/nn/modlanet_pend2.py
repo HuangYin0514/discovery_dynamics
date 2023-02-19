ba952d203097e6f2a16ff0a70344c7fe744e677c
drhuang <1071324110@qq.com>
2023-02-19 21:50:26 +0800

    changes

--- learner/nn/modlanet_pend2.py
+++ learner/nn/modlanet_pend2.py
@@ -79,7 +79,7 @@ class ModLaNet_pend2(LossNN):
         self.global4v = GlobalVelocityTransform(input_dim=self.dim,
                                                 hidden_dim=16,
                                                 output_dim=self.global_dim,
-                                                num_layers=1, act=nn.Tanh)
+                                                num_layers=2, act=nn.Tanh)
         self.Potential1 = PotentialEnergyCell(input_dim=self.global_dim,
                                               hidden_dim=50,
                                               output_dim=1,
