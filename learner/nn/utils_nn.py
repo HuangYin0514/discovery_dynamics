90a9e966cdc6472722917be3d4ee0c46e0774f7d
drhuang <1071324110@qq.com>
2023-02-19 23:13:30 +0800

    changes

--- learner/nn/utils_nn.py
+++ learner/nn/utils_nn.py
@@ -13,8 +13,8 @@ def weights_init_xavier_normal(m):
     classname = m.__class__.__name__
     if classname.find("Linear") != -1:  # find the linear layer class
         nn.init.xavier_normal_(m.weight)
-        # if m.bias is not None:
-        #     nn.init.constant_(m.bias, 0.0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
 
 
 def weights_init_orthogonal_normal(m):
