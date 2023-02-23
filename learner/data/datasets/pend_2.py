b0b87a5530aa66becc23dcd66cc42775af004e65
drhuang <1071324110@qq.com>
2023-02-23 17:41:06 +0800

    changes

--- learner/data/datasets/pend_2.py
+++ learner/data/datasets/pend_2.py
@@ -140,13 +140,13 @@ class Pendulum2(BaseBodyDataset, nn.Module):
     def random_config(self, num):
         x0_list = []
         for i in range(num):
-            max_momentum = 1.
-            x0 = torch.zeros((self.obj * 2))
+            max_momentum = 10.
+            y0 = torch.zeros((self.obj * 2))
             for i in range(self.obj):
-                theta = (2 * np.pi) * torch.rand(1, ) + 0  # [0, 2pi]
-                momentum = (2 * torch.rand(1, ) - 1) * max_momentum  # [-1, 1]*max_momentum
-                x0[i] = theta
-                x0[i + self.obj] = momentum
-            x0_list.append(x0)
+                theta = (2 * torch.rand(1)) * torch.pi
+                momentum = (2 * torch.rand(1) - 1) * max_momentum
+                y0[i] = theta
+                y0[i + self.obj] = momentum
+            x0_list.append(y0)
         x0 = torch.stack(x0_list)
-        return x0.to(self.Device)
+        return x0.clone().detach()
