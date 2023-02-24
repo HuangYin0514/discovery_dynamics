b050e12e9b633dc77f7278d51b5d2d7ea4178cd3
drhuang <1071324110@qq.com>
2023-02-24 16:27:27 +0800

    changes

--- learner/data/datasets/pend_2_L.py
+++ learner/data/datasets/pend_2_L.py
@@ -130,7 +130,9 @@ class Pendulum2_L(BaseBodyDataset, nn.Module):
     def random_config(self, num):
         x0_list = []
         for i in range(num):
-            max_momentum = 1.
+            max_momentum = 10.
+            if num == self.test_num:
+                max_momentum = 1.
             y0 = np.zeros(self.obj * 2)
             for i in range(self.obj):
                 theta = (2 * np.random.rand()) * np.pi
