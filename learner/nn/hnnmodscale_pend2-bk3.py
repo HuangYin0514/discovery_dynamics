f407941f53157343378c348820a37cb55ee7a09e
drhuang <1071324110@qq.com>
2023-02-27 16:02:17 +0800

    changes

---
+++ learner/nn/hnnmodscale_pend2-bk3.py
@@ -0,0 +1,103 @@
+# encoding: utf-8
+"""
+@author: Yin Huang
+@contact: hy1071324110@gmail.com
+@time: 2023/2/12 9:56 AM
+@desc:
+"""
+
+# encoding: utf-8
+"""
+@author: Yin Huang
+@contact: hy1071324110@gmail.com
+@time: 2023/1/18 4:41 PM
+@desc:
+"""
+import torch
+
+from ._base_module import LossNN
+from ..integrator import ODESolver
+from ..utils import dfx
+
+
+class HnnModScale_pend2(LossNN):
+    """
+    Mechanics neural networks.
+    """
+
+    def __init__(self, obj, dim, num_layers=None, hidden_dim=None):
+        super(HnnModScale_pend2, self).__init__()
+
+        q_dim = int(obj * dim)
+        p_dim = int(obj * dim)
+
+        self.obj = obj
+        self.dim = dim
+        self.dof = int(obj * dim)
+
+        self.mass = torch.nn.Linear(1, 1, bias=False)
+        torch.nn.init.ones_(self.mass.weight)
+
+    def M(self, x):
+        N = self.obj
+        M = torch.zeros((x.shape[0], N, N), dtype=self.Dtype, device=self.Device)
+        for i in range(N):
+            for k in range(N):
+                m_sum = 0
+                j = i if i >= k else k
+                for tmp in range(j, N):
+                    m_sum += 1.0
+                M[:, i, k] = torch.cos(x[:, i] - x[:, k]) * m_sum
+        return M
+
+    def Minv(self, x):
+        return torch.linalg.inv(self.M(x))
+
+    def forward(self, t, coords):
+        __x, __p = torch.chunk(coords, 2, dim=-1)
+        coords = torch.cat([__x % (2 * torch.pi), __p], dim=-1).clone().detach().requires_grad_(True)
+
+        coords = coords.clone().detach().requires_grad_(True)
+        bs = coords.size(0)
+        q, p = coords.chunk(2, dim=-1)  # (bs, q_dim) / (bs, p_dim)
+
+        # Calculate the potential energy for i-th element ------------------------------------------------------------
+        U = 0.
+        y = 0.
+        for i in range(self.obj):
+            y = y - torch.cos(q[:, i])
+            U = U + 9.8 * y
+
+        # Calculate the kinetic --------------------------------------------------------------
+        # T = self.dataset.kinetic(torch.cat([x, p], dim=-1).reshape(-1))
+
+        T = 0.
+        v = torch.matmul(self.Minv(q), p.unsqueeze(-1))
+        T = 0.5 * torch.matmul(p.unsqueeze(1), v).squeeze(-1).squeeze(-1)
+
+        # Calculate the Hamilton Derivative --------------------------------------------------------------
+        H = U + T
+        dqH = dfx(H.sum(), q)
+        dpH = dfx(H.sum(), p)
+
+        v_global = self.Minv(q).matmul(p.unsqueeze(-1)).squeeze(-1)
+
+        # Calculate the Derivative ----------------------------------------------------------------
+        # dq_dt = torch.zeros((bs, self.dof), dtype=self.Dtype, device=self.Device)
+        # dp_dt = torch.zeros((bs, self.dof), dtype=self.Dtype, device=self.Device)
+        # for i in range(self.obj):
+        #     dq_dt[:, i * self.dim:(i + 1) * self.dim] = dpH[:, i * self.dim: (i + 1) * self.dim]
+        #     dp_dt[:, i * self.dim:(i + 1) * self.dim] = -dqH[:, i * self.dim:(i + 1) * self.dim]
+        # dz_dt = torch.cat([dq_dt, dp_dt], dim=-1)
+        dq_dt = torch.zeros((bs, self.dof), dtype=self.Dtype)
+        dp_dt = torch.zeros((bs, self.dof), dtype=self.Dtype)
+
+        dq_dt = dpH
+        dp_dt = -dqH
+
+        dz_dt = torch.cat([dq_dt, dp_dt], dim=-1)
+        return dz_dt
+
+    def integrate(self, X0, t):
+        out = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+        return out
--- learner/nn/hnnmodscale_pend2.py
+++ learner/nn/hnnmodscale_pend2.py
@@ -5,6 +5,10 @@
 @time: 2023/2/12 9:56 AM
 @desc:
 """
+from torch import nn
+
+from .mlp import MLP
+from .utils_nn import ReshapeNet
 
 # encoding: utf-8
 """
@@ -18,6 +22,49 @@ import torch
 from ._base_module import LossNN
 from ..integrator import ODESolver
 from ..utils import dfx
+
+
+class GlobalPositionTransform(nn.Module):
+    """Doing coordinate transformation using a MLP"""
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1, act=nn.Tanh):
+        super(GlobalPositionTransform, self).__init__()
+
+        self.mlp = MLP(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim,
+                       num_layers=num_layers,
+                       act=act)
+
+    def forward(self, x, x_0):
+        y = self.mlp(x) + x_0
+        return y
+
+
+class MassNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim=30, num_layers=3, act=nn.Tanh):
+        super(MassNet, self).__init__()
+
+        self.net = nn.Sequential(
+            MLP(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=input_dim * input_dim,
+                num_layers=num_layers, act=act),
+            ReshapeNet(-1, input_dim, input_dim)
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        return out
+
+
+class PotentialEnergyCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1, act=nn.Tanh):
+        super(PotentialEnergyCell, self).__init__()
+
+        self.mlp = MLP(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim,
+                       num_layers=num_layers,
+                       act=act)
+
+    def forward(self, x):
+        y = self.mlp(x)
+        return y
 
 
 class HnnModScale_pend2(LossNN):
@@ -34,6 +81,26 @@ class HnnModScale_pend2(LossNN):
         self.obj = obj
         self.dim = dim
         self.dof = int(obj * dim)
+
+        self.global_dim = 2
+        self.global_dof = int(obj * self.global_dim)
+
+        self.mass_net = MassNet(input_dim=self.dof, num_layers=1, hidden_dim=50, act=nn.Tanh)
+        self.global4x = GlobalPositionTransform(input_dim=self.dim,
+                                                hidden_dim=16,
+                                                output_dim=self.global_dim,
+                                                num_layers=1, act=nn.Tanh)
+        self.Potential1 = PotentialEnergyCell(input_dim=self.global_dim,
+                                              hidden_dim=50,
+                                              output_dim=1,
+                                              num_layers=1, act=nn.Tanh)
+        self.Potential2 = PotentialEnergyCell(input_dim=self.global_dim * 2,
+                                              hidden_dim=50,
+                                              output_dim=1,
+                                              num_layers=1, act=nn.Tanh)
+
+        self.co1 = torch.nn.Parameter(torch.ones(1, dtype=self.Dtype, device=self.Device) * 0.5)
+        self.co2 = torch.nn.Parameter(torch.ones(1, dtype=self.Dtype, device=self.Device) * 0.5)
 
         self.mass = torch.nn.Linear(1, 1, bias=False)
         torch.nn.init.ones_(self.mass.weight)
@@ -61,12 +128,35 @@ class HnnModScale_pend2(LossNN):
         bs = coords.size(0)
         q, p = coords.chunk(2, dim=-1)  # (bs, q_dim) / (bs, p_dim)
 
+        # position transformations ----------------------------------------------------------------
+        x_global = torch.zeros((bs, self.global_dof), dtype=self.Dtype, device=self.Device)
+        x_origin = torch.zeros((bs, self.global_dof), dtype=self.Dtype, device=self.Device)
+        for i in range(self.obj):
+            for j in range(i):
+                x_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim] += x_global[:, (j) * self.global_dim:
+                                                                                             (j + 1) * self.global_dim]
+            x_global[:, (i) * self.global_dim: (i + 1) * self.global_dim] = self.global4x(
+                q[:, (i) * self.dim: (i + 1) * self.dim],
+                x_origin[:, (i) * self.global_dim: (i + 1) * self.global_dim])
+
         # Calculate the potential energy for i-th element ------------------------------------------------------------
         U = 0.
-        y = 0.
         for i in range(self.obj):
-            y = y - torch.cos(q[:, i])
-            U = U + 9.8 * y
+            U += self.co1 * self.mass(
+                self.Potential1(x_global[:, i * self.global_dim: (i + 1) * self.global_dim]))
+
+        for i in range(self.obj):
+            for j in range(i):
+                x_ij = torch.cat(
+                    [x_global[:, i * self.global_dim: (i + 1) * self.global_dim],
+                     x_global[:, j * self.global_dim: (j + 1) * self.global_dim]],
+                    dim=1)
+                x_ji = torch.cat(
+                    [x_global[:, j * self.global_dim: (j + 1) * self.global_dim],
+                     x_global[:, i * self.global_dim: (i + 1) * self.global_dim]],
+                    dim=1)
+                U += self.co2 * (
+                        0.5 * self.mass(self.Potential2(x_ij)) + 0.5 * self.mass(self.Potential2(x_ji)))
 
         # Calculate the kinetic --------------------------------------------------------------
         # T = self.dataset.kinetic(torch.cat([x, p], dim=-1).reshape(-1))
