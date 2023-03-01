0f77be4533ab67974825ee512153bdcafb3bf58e
drhuang <1071324110@qq.com>
2023-03-01 18:01:47 +0800

    changes

--- learner/data/datasets/pend_2_L.py
+++ learner/data/datasets/pend_2_L.py
@@ -150,7 +150,7 @@ class Pendulum2_L(BaseBodyDataset, nn.Module):
         # the double pendulum task. Therefore, use dopri5 to generate training data.
         if len(t) == len(self.test_t):
             # test stages
-            x = ODESolver(self, x0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
+            x = ODESolver(self, x0, t, method='dopri5').permute(1, 0, 2)  # (T, D) dopri5 rk4
         else:
             # train stages
             x = ODESolver(self, x0, t, method='dopri5').permute(1, 0, 2)  # (T, D) dopri5 rk4
