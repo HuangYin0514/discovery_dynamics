2c29b593b6a558e32d208c099f17d80a9630ac26
drhuang <1071324110@qq.com>
2023-03-08 09:24:45 +0800

    changes

--- learner/nn/baseline_pend2_dae.py
+++ learner/nn/baseline_pend2_dae.py
@@ -15,6 +15,7 @@ from torch import nn
 from ._base_module import LossNN
 from .mlp import MLP
 from ..integrator import ODESolver
+from ..utils.common_utils import enable_grad
 
 
 class Baseline_pend2_dae(LossNN):
@@ -30,11 +31,11 @@ class Baseline_pend2_dae(LossNN):
         self.baseline = MLP(input_dim=obj * dim * 2, hidden_dim=200, output_dim=obj * dim * 2, num_layers=1,
                             act=nn.Tanh)
 
+    @enable_grad
     def forward(self, t, coords):
-        with torch.enable_grad():
-            coords = coords.clone().detach().requires_grad_(True)
-            out = self.baseline(coords)
-            return out
+        coords = coords.clone().detach().requires_grad_(True)
+        out = self.baseline(coords)
+        return out
 
     def integrate(self, X0, t):
         coords = ODESolver(self, X0, t, method='rk4').permute(1, 0, 2)  # (T, D) dopri5 rk4
