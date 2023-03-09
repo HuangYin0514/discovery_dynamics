42a1f331caad7b1491f2f9cd6c2ff3b5a60baf64
drhuang <1071324110@qq.com>
2023-03-09 15:53:05 +0800

    changes

--- learner/nn/analytical_pend2_dae.py
+++ learner/nn/analytical_pend2_dae.py
@@ -59,7 +59,7 @@ class Analytical_pend2_dae(LossNN):
         lam = torch.linalg.solve(L, R)  # (2, 1)
 
         bs = x.shape[0]
-        a = torch.cat([phi_qq @ v.unsqueeze(-1) , phi_qq @ v.unsqueeze(-1) ], dim=-1).reshape(-1, 4)+1
+        a = torch.cat([phi_qq @ v.unsqueeze(-1), phi_qq @ v.unsqueeze(-1)], dim=-1).reshape(-1, 4) + 1
         return torch.cat([v, a], dim=-1)
 
     def Minv(self, q):
