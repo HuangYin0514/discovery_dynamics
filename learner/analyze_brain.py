4982178c9dc7e228c6bd1404837d595f57a06d53
drhuang <1071324110@qq.com>
2023-03-09 20:34:04 +0800

    changes

--- learner/analyze_brain.py
+++ learner/analyze_brain.py
@@ -89,6 +89,10 @@ class AnalyzeBrain:
         pred_kinetic_eng = self.kinetic_fn(net_pred)
         pred_potential_eng = self.potential_fn(net_pred)
 
+        print(sum(true_eng- pred_eng))
+        print(sum(true_kinetic_eng- pred_kinetic_eng))
+        print(sum(true_potential_eng- pred_potential_eng))
+
         t = t.detach().cpu().numpy()
 
         ground_true = ground_true.detach().cpu().numpy()
