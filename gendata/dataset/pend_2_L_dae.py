de88aecdf4c08aa57949467e9f266ad1edbc8138
drhuang <1071324110@qq.com>
2023-03-09 12:13:09 +0800

    changes

--- gendata/dataset/pend_2_L_dae.py
+++ gendata/dataset/pend_2_L_dae.py
@@ -77,7 +77,7 @@ class Pendulum2_L_dae(BaseBodyDataset, nn.Module):
         # 求解 lam ----------------------------------------------------------------
         L = phi_q @ Minv @ phi_q.permute(0, 2, 1)
         # R = phi_q @ Minv @ F.unsqueeze(-1) + phi_qq @ v.unsqueeze(-1)  # (2, 1)
-        R = phi_q @ Minv @ F.unsqueeze(-1) + torch.tensor([[5], [6]], dtype=self.Dtype, device=self.Device).reshape(1, 2, 1).repeat(bs, 1, 1)
+        R = phi_qq @ v.unsqueeze(-1)+ torch.tensor([[5], [6]], dtype=self.Dtype, device=self.Device).reshape(1, 2, 1).repeat(bs, 1, 1)
         lam = torch.linalg.solve(L, R)  # (2, 1)
         # ----------------------------------------------------------------
 
