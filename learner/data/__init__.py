bb35bfbcade9827f9e90386ad0d06b2a0b983028
drhuang <1071324110@qq.com>
2023-03-09 21:26:37 +0800

    changes

--- learner/data/__init__.py
+++ learner/data/__init__.py
@@ -40,7 +40,7 @@ def get_dataloader(data_name, dataset_path, num_workers=0, **kwargs):
     )
 
     test_loader = torch.utils.data.DataLoader(
-        val_set, batch_size=5, shuffle=False,
+        val_set, batch_size=len_val_set, shuffle=False,
         num_workers=num_workers, collate_fn=test_collate_fn
     )
 
