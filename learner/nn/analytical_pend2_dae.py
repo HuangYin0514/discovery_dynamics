5fb32118b8c855b4c704c86c57a72b2ae3df3499
drhuang <1071324110@qq.com>
2023-03-09 20:48:42 +0800

    changes

--- learner/nn/analytical_pend2_dae.py
+++ learner/nn/analytical_pend2_dae.py
@@ -69,7 +69,7 @@ class Analytical_pend2_dae(LossNN):
         phiq_Minv = torch.bmm(phi_q, Minv)  # (bs,2,4)
         L = torch.bmm(phiq_Minv, phi_q.permute(0, 2, 1))
         # R = torch.matmul(phiq_Minv, F.unsqueeze(-1)) + torch.matmul(phi_qq, v.unsqueeze(-1))  # (2, 1)
-        R = torch.bmm(phiq_Minv, F.unsqueeze(-1))  # (2, 1)
+        R = torch.bmm(phiq_Minv, F.unsqueeze(-1))  + torch.bmm(phi_qq, v.unsqueeze(-1))  # (2, 1)
 
         L = L.reshape(bs, 2, 2)
         R = R.reshape(bs, 2, 1)
