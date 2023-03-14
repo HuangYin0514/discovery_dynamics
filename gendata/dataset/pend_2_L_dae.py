58245123608155d78ea53767fac54695d0874dce
drhuang <1071324110@qq.com>
2023-03-14 15:13:38 +0800

    changes

--- gendata/dataset/pend_2_L_dae.py
+++ gendata/dataset/pend_2_L_dae.py
@@ -42,12 +42,12 @@ class Pendulum2_L_dae(BaseBodyDataset, nn.Module):
 
         t0 = 0.
 
-        t_end = 6.0
+        t_end = 3.0
         dt = 0.01
         _time_step = int((t_end - t0) / dt)
         self.t = torch.linspace(t0, t_end, _time_step)
 
-        t_end = 15.0
+        t_end = 8.0
         dt = 0.01
         _time_step = int((t_end - t0) / dt)
         self.test_t = torch.linspace(t0, t_end, _time_step)
